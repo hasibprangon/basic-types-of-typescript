4bd37996cd1a839d4763d742385225b0828aa06a
hasibprangon <hasibulhossainp09@gmail.com>
2025-05-01 20:14:09 +0600

    function

---
+++ src/1.6.ts
@@ -0,0 +1,38 @@
+// Learning Function
+// Normal Function
+
+function add(num1: number, num2: number): number {
+    return num1 + num2;
+}
+// add(2, 2)
+
+// Arrow Function
+
+const addArrow = (num1: number, num2: number): number => num1 + num2;
+
+// object ==> function ==> method
+const person: {
+    myName: string;
+    balance: number;
+    addBalance(balance : number) : number;
+} = {
+    myName: 'Prangon',
+    balance: 0,
+    addBalance(balance: number): number {
+        return this.balance + balance;
+    }
+};
+
+
+const person2 : {
+    myName : string;
+    balance : number;
+    addBalance2(balance : number) : string
+} = {
+    myName : 'Hasib',
+    balance : 200,
+    addBalance2(balance) {
+        return `My balance is ${this.balance + balance}`;
+    }
+};
+
