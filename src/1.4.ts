d1b826b06930341980c431c8779380e7a6dcfeba
hasibprangon <hasibulhossainp09@gmail.com>
2025-05-01 18:31:03 +0600

    basic dataTypes

---
+++ src/1.4.ts
@@ -0,0 +1,40 @@
+//Basic Data Types
+
+// explicit data types + primitive
+
+//string
+
+let firstName : string = 'Hasib';
+
+// number
+
+let roll : number = 100;
+
+// boolean
+
+let isAdmin : boolean = true;
+
+// undefined
+
+let x : undefined = undefined;
+
+// null
+
+let y : null = null;
+
+// NonPrimitive
+
+// array
+
+let myName : string[] = ['Hasib', 'Prangon'];
+
+let marks : number[] = [50, 60]
+
+// tuple --> array --> order --> type of values
+
+let coordinates : [number, number] = [1, 10];
+
+let ageName : [number, string] = [25, 'Hasib']
+
+
+
